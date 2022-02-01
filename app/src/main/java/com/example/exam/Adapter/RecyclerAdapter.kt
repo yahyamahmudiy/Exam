b8c7f07f12a866aba891b730fe2e3102d0fd01d8
Yahya Mahmudiy <mustofamahmudiy@gmail.com>
2022-02-01 12:01:55 +0500

    I have added recylcer view and item tablet layout

--- app/src/main/java/com/example/exam/Adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/exam/Adapter/RecyclerAdapter.kt
@@ -12,44 +12,23 @@ import com.example.exam.R
 
 class RecyclerAdapter(var context: Context, var members: ArrayList<Member>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    companion object {
-        private const val TYPE_HEADER = 0
-        private const val TYPE_ITEMS = 1
-        private const val TYPE_FOOTER = 2
-    }
 
     var isLoading:Boolean = false
 
-    fun setIsLoading(value:Boolean) {
-        isLoading = value
-    }
+
 
     override fun getItemCount(): Int {
-        return if(!isLoading) members.size + 1 else members.size + 2
+        return  members.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-
-        if(viewType == TYPE_ITEMS){
             val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout_phone, parent, false)
             return CustomViewHolder(view)
-        }else{
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout_phone, parent, false)
-            return CustomViewHolder(view)
-        }
     }
 
 
 
-    override fun getItemViewType(position: Int): Int {
-        if(position == 0) {
-            return TYPE_HEADER
-        } else if(position == members.size + 2) {
-            return TYPE_FOOTER
-        }
 
-        return TYPE_ITEMS
-    }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
--- app/src/main/java/com/example/exam/MainActivity.kt
+++ app/src/main/java/com/example/exam/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.exam
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.exam.Adapter.RecyclerAdapter
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     var context: Context? = null
     var recyclerView: RecyclerView? = null
+    var recyclerViewAdapter:RecyclerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +23,28 @@ class MainActivity : AppCompatActivity() {
         initViews()
     }
 
+
+
     fun initViews() {
         context = this
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView!!.setLayoutManager(GridLayoutManager(context, 1))
+
+        recyclerView!!.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val gridLayoutManager = recyclerView.getLayoutManager() as GridLayoutManager
+                val totalItem = gridLayoutManager.itemCount
+                val lastVisible = gridLayoutManager.findLastVisibleItemPosition()
+                val endHasBeenReached = lastVisible + 5 >= totalItem
+                if (totalItem > 0 && endHasBeenReached) {
+                    Toast.makeText(context, "Loading more ... ", Toast.LENGTH_SHORT).show()
+                    loadMoreFoods()
+                    recyclerViewAdapter?.notifyDataSetChanged();
+                }
+            }
+        })
+
         val members: ArrayList<Member> = java.util.ArrayList<Member>()
 
         for (i in 0..10) {
@@ -38,6 +59,18 @@ class MainActivity : AppCompatActivity() {
         refreshAdapter(members)
     }
 
+
+    private fun loadMoreFoods(){
+        val members: ArrayList<Member> = java.util.ArrayList<Member>()
+
+        members.add(Member(R.drawable.img, "Hamburger"))
+        members.add(Member(R.drawable.img_1, "Hot Dog"))
+        members.add(Member(R.drawable.img_2, "Stake"))
+        members.add(Member(R.drawable.img_3, "Pizza"))
+        members.add(Member(R.drawable.img_4, "Salad"))
+
+
+    }
     fun refreshAdapter(members: ArrayList<Member>?) {
         val adapter = RecyclerAdapter(context!!, members!!)
         recyclerView!!.adapter = adapter
